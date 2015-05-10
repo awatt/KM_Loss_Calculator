2757c46cdbbdb986e9a5d27405860bf1e3555406
Andrew Watt <awatt11215@gmail.com>
2015-05-10 16:48:18 -0400

    first commit

---
+++ client/app/factories/trades/trades.service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('kmLossCalculatorApp')
+  .factory('trades', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
