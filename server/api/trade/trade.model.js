f7c9be4ef5456c49d9d5fadd87e2ce2e663037e0
Andrew Watt <awatt11215@gmail.com>
2015-05-12 22:34:31 -0400

    added angular material elements

--- server/api/trade/trade.model.js
+++ server/api/trade/trade.model.js
@@ -19,19 +19,3 @@ var TradeSchema = new Schema({
 });
 
 module.exports = mongoose.model('Trade', TradeSchema);
-
-
-// {  
-//    "_id":ObjectId("5551184da767d03bf16bed72"),
-//    "securityType":"ISIN",
-//    "securityID":"US71654V4086",
-//    "securityDescription":"PETROLEO BRASILEIRO-SPON ADR",
-//    "account":"Account 1",
-//    "tradeDate":"6/30/10",
-//    "valueDate":"7/6/10",
-//    "transactionType":"BUY",
-//    "quantity":"2,600.00",
-//    "pricePerShare":34.4,
-//    "mVofTrade":"89,431.34",
-//    "currency":"USD"
-// }
